21c0491c88631f1c2a359f49648bf0be420f2175
yui.s <y.u.i020928@gmail.com>
2022-04-13 01:35:08 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 杉田優衣 on 2022/04/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
